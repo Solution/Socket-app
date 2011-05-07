3dacb242b78230864fde2c4461969da6bb6f32f7
Patrik Tomášik <solution@efine.cz>
2011-05-07 11:31:28 +0200

    Big update

---
+++ Request.cpp
@@ -0,0 +1,25 @@
+/* 
+ * File:   Request.cpp
+ * Author: patriktomasik
+ *
+ * Created on 1. květen 2011, 10:52
+ */
+
+//#include "Request.h"
+
+
+/*Request::Request(char* data)
+{
+    
+}
+
+std::string Request::getHeader(std::string key)
+{
+    
+}
+
+vector<std::string, std::string> Request::getHeaders()
+{
+    
+}
+ */
---
+++ Request.h
@@ -0,0 +1,22 @@
+/* 
+ * File:   Request.h
+ * Author: patriktomasik
+ *
+ * Created on 1. květen 2011, 10:52
+ */
+
+#ifndef REQUEST_H
+#define	REQUEST_H
+/*class Request
+{
+private:
+    vector<std::string, std::string> header;
+public:
+    Request(char*);
+    std::string getHeader(std::string);
+    vector<std::string, std::string> getHeaders();
+};
+
+*/
+#endif	/* REQUEST_H */
+
